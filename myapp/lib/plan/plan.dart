080beaafc069cf9347da2d4c33aec18ecc400014
Daniel Jo <sungkeun.jo@gmail.com>
2021-01-06 13:40:09 -0800

    Created container for left section

--- myapp/lib/plan/plan.dart
+++ myapp/lib/plan/plan.dart
@@ -2,93 +2,72 @@ import 'package:flutter/material.dart';
 
 class Plan extends StatelessWidget {
 
-  Widget build(BuildContext context){
-    return Scaffold(
-
-      // Plan Your Scheulde Title
-      body: Padding(
+  // Left portion of the view
+  // Browse, Schedule, Ratings
+  final left = Container(
+    child: Padding(
         padding: EdgeInsets.fromLTRB(30, 30, 30, 30),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text("Plan",
-              style: TextStyle(fontSize: 50)
-            ),
-
-          SizedBox(height: 30.0),
-
-          // Browse Tab
-          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              TextButton(
-                onPressed: (){},
-                child: Text(
-                  "Browse",
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.black,
-                  )
+              Text("Plan Your Schedule", style: TextStyle(fontSize: 50)),
+              SizedBox(height: 30.0),
+              Row(children: <Widget>[
+                TextButton(
+                  onPressed: () {},
+                  child: Text("Browse",
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.black,
+                      )),
                 ),
-              ),
-
-              SizedBox(width: 30.0),
-              
-              Text(
-                "|",
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.black,
+                SizedBox(width: 30.0),
+                Text("|",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.black,
+                    )),
+                SizedBox(width: 30.0),
+                TextButton(
+                  onPressed: () {},
+                  child: Text("Schedule",
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.black,
+                      )),
+                ),
+                SizedBox(width: 30.0),
+                Text("|",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.black,
+                    )),
+                SizedBox(width: 30.0),
+                TextButton(
+                  onPressed: () {},
+                  child: Text("Ratings",
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.black,
+                      )),
                 )
-              ),
+              ]),
+              SizedBox(height: 30.0),
+              Text("Search Bar"),
+              SizedBox(height: 30.0),
+            ])),
+  );
 
-              SizedBox(width: 30.0),
+  // Calendar view needed
 
-              // Schedule Tab
-              TextButton(
-                onPressed: (){},
-                child: Text(
-                  "Schedule",
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.black,
-                  )
-                ),
-              ),
-
-              SizedBox(width: 30.0),
-
-               Text(
-                "|",
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.black,
-                )
-              ),
-
-              SizedBox(width: 30.0),
-
-              // Ratings Tab
-              TextButton(
-                onPressed: (){},
-                child: Text(
-                  "Ratings",
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.black,
-                  )
-                ),
-              )
-             ] 
-            ),
-          SizedBox(height: 30.0),
-
-          Text("Search Bar"),
-
-          SizedBox(height: 30.0),
-
-          ]
-        )
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: new Container(
+      child: new Row(
+        children: <Widget>[
+          left,
+        ],
       ),
-    );  
+    ));
   }
 }
