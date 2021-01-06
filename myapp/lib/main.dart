254d6e55a3f8983ed176604e0c9c5321513401f8
Daniel Jo <sungkeun.jo@gmail.com>
2021-01-05 16:21:53 -0800

    Created myapp and a header with tabs

---
+++ myapp/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+// Import our personal widgets here
+import 'package:myapp/header.dart';
+
+
+// Run the app
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'University Scheduling Application',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Header(),
+    );
+  }
+}
