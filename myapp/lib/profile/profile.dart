5a5fa97e88d0e5509b3438221a2b7991108e3a30
Kevin Patel <kevinjaypatel@gmail.com>
2021-01-09 15:54:09 -0800

    added a profile module

---
+++ myapp/lib/profile/profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      Text("Hello Profile"),
+    );
+  }
+}
