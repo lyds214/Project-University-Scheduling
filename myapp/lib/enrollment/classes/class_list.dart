0bdbf89ceb17ea7e1eaa48323bcff14c4bc372c6
lyds214 <lydiayang92@gmail.com>
2021-01-08 22:19:59 -0800

    Added a new file
    -added class_list.dart to break down my code into smaller parts.

---
+++ myapp/lib/enrollment/classes/class_list.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+
+class ClassList extends StatefulWidget{
+  @override
+  ClassListState createState() => ClassListState();
+}
+
+class ClassListState extends State<ClassList>
+{
+  Widget build(BuildContext context)
+  {
+    return TabBarView(
+        children: <Widget>
+        [
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("A", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("B", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("C", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("D", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("E", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("F", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("G", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("H", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("I", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("J", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("K", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("L", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("M", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("N", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("O", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("P", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("Q", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("R", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("S", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("T", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("U", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("V", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("W", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("X", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("Y", style: TextStyle(fontSize: 30),),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 40, 10, 10),
+            child: Text("Z", style: TextStyle(fontSize: 30),),
+          ),                  
+        ]
+      );
+  }
+}
+ 
