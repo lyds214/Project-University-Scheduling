694d7eaf11fa20b7fea34cb71631a2634888e8b6
Kevin Patel <kevinjaypatel@gmail.com>
2021-01-10 18:36:16 -0800

    added a Button for the user in the inbox module

--- myapp/lib/inbox/inbox.dart
+++ myapp/lib/inbox/inbox.dart
@@ -3,6 +3,6 @@ import 'package:myapp/inbox/profile/profile_card.dart';
 
 class Inbox extends StatelessWidget {
   Widget build(BuildContext context) {
-    return Text('Notification Module (Placeholder)');
+    return ProfileCard();
   }
 }
--- myapp/lib/inbox/profile/profile_card.dart
+++ myapp/lib/inbox/profile/profile_card.dart
@@ -4,9 +4,20 @@ class ProfileCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        color: Colors.grey,
-        child: Text('User'),
+      child: ButtonTheme(
+        minWidth: 230.0,
+        height: 125.0,
+        buttonColor: Colors.grey[300],
+        child: RaisedButton(
+          onPressed: () {},
+          child: Text(
+            'User',
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.black, 
+            ),
+          ),
+        ),
       ),
     );
   }
