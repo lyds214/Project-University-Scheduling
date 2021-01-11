df0f8e5c3635521c30a8805a14ea8c1ccdfc3caa
Brenden Smith <brendensmith01@gmail.com>
2021-01-10 19:37:23 -0800

    Reorganized catalog files

--- myapp/lib/enrollment/enrollment.dart
+++ myapp/lib/enrollment/enrollment.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 // Three views
-import 'package:myapp/enrollment/catalog.dart';
+import 'package:myapp/enrollment/catalog/catalog.dart';
 import 'package:myapp/enrollment/classes/class_card.dart';
 import 'package:myapp/enrollment/major_requirements.dart';
 import 'package:myapp/enrollment/semester.dart';
