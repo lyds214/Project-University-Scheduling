052550826c9de21cef0222794f3025dfb3bf8859
Brenden Smith <brendensmith01@gmail.com>
2021-01-11 12:47:34 -0800

    Reorganized item types & fixed dependencies

---
+++ myapp/lib/classes/items.dart
@@ -0,0 +1,35 @@
+class CollegeItem { // Item to create overall college
+  String acronym;
+  String name;
+  SubjectItem subject;
+
+  CollegeItem({this.acronym, this.name, this.subject});
+}
+
+class SubjectItem { // Abstract tem to create subject
+
+  String acronym;
+  String subject;
+  _ClassItem subClass;
+
+  SubjectItem({this.acronym, this.subject, this.subClass});
+}
+
+
+class _ClassItem { // Abstract item to create class
+
+  String classNumber;
+  String classTitle;
+  String genEdReq;
+  _ClassItem section;
+  _ClassItem({this.classNumber, this.classTitle, this.section});
+}
+
+class _SectionItem { // Abstract item to create section
+  String section;
+  
+  _SectionItem({this.section});
+}
+
+
+
