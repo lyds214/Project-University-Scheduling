21fecba969885f8c2e5ac5cd1cc7ea96306b3b2c
Daniel Jo <sungkeun.jo@gmail.com>
2021-01-13 11:36:51 -0800

    Outlined box for calendar

--- myapp/lib/plan/weekview.dart
+++ myapp/lib/plan/weekview.dart
@@ -9,60 +9,54 @@ class WeekViewState extends State<WeekView> {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-        child: Padding(
-      padding: EdgeInsets.fromLTRB(30, 30, 30, 30),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          HoursColumn(),
-          DayWidget('Monday'),
-          DayWidget('Tuesday'),
-          DayWidget('Wednesday'),
-          DayWidget('Thursday'),
-          DayWidget('Friday'),
-          DayWidget('Saturday'),
-        ],
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(30, 30, 30, 30),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[Text('Current Schedule')],
+            ),
+            Container(
+              decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  HoursColumn(),
+                  DayWidget('Monday'),
+                  DayWidget('Tuesday'),
+                  DayWidget('Wednesday'),
+                  DayWidget('Thursday'),
+                  DayWidget('Friday'),
+                  DayWidget('Saturday'),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
 
 class HoursColumn extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(children: <Widget>[
-      Spacer(),
       Text('7:00AM'),
-      Spacer(),
       Text('8:00AM'),
-      Spacer(),
       Text('9:00AM'),
-      Spacer(),
       Text('10:00AM'),
-      Spacer(),
       Text('11:00AM'),
-      Spacer(),
       Text('12:00PM'),
-      Spacer(),
       Text('1:00PM'),
-      Spacer(),
       Text('2:00PM'),
-      Spacer(),
       Text('3:00PM'),
-      Spacer(),
       Text('4:00PM'),
-      Spacer(),
       Text('5:00PM'),
-      Spacer(),
       Text('6:00PM'),
-      Spacer(),
       Text('7:00PM'),
-      Spacer(),
       Text('8:00PM'),
-      Spacer(),
       Text('9:00PM'),
-      Spacer(),
       Text('10:00PM'),
-      Spacer(),
       Text('11:00PM'),
     ]);
   }
